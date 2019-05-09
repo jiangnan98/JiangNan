48937b04b4591b290e3bc35fde1714b79e84536d
liliubing <liliubing@qq.com>
2019-05-09 09:50:56 +0800

    点火线圈商品上线

---
+++ src/main/java/com/bing/controller/IgnController.java
@@ -0,0 +1,11 @@
+package com.bing.controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: NinetyOne
+ * Date: 2019/5/8
+ * Time: 14:36
+ * To change this template use File | Setting | File Template.
+ **/
+public class IgnController {
+}
---
+++ src/main/java/com/bing/util/ExcelImgUtils/GetImgFromExcel.java
@@ -0,0 +1,11 @@
+package com.bing.util.ExcelImgUtils;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: NinetyOne
+ * Date: 2019/5/8
+ * Time: 16:05
+ * To change this template use File | Setting | File Template.
+ **/
+public class GetImgFromExcel {
+}
